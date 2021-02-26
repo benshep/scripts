b01109099cdb5f631fb952c676ed51da5a18a22d
Ben Shepherd <bjashepherd@gmail.com>
2021-02-26 11:57:49 +0000

    [random_cd] show tracklist, and wait for 5 seconds after scrobbling

--- random_cd.py
+++ random_cd.py
@@ -4,7 +4,7 @@ import os
 from random import choice
 from lastfm import lastfm  # contains secrets, so don't show them here
 from phrydy import MediaFile  # to get media data
-from time import time
+from time import time, sleep
 
 music_folder = os.path.join(os.environ['UserProfile'], 'Music')
 exclude_prefixes = tuple(open(os.path.join(music_folder, 'not_cd_folders.txt')).read().split('\n'))
@@ -19,11 +19,11 @@ for folder, folder_list, file_list in os.walk(music_folder):
 folder = choice(cd_folders)
 print(folder)
 start_time = int(time())
-num_tracks = input('Scrobble up to track [auto]: ')
-
 os.chdir(os.path.join(music_folder, folder))
 track_list = sorted([MediaFile(file) for file in os.listdir() if file.lower().endswith(('mp3', 'm4a', 'wma'))],
                     key=lambda media: int(media.track))
+[print(f'{media.track:2d}. {media.title}') for media in track_list]
+num_tracks = input('Scrobble up to track [auto]: ')
 num_tracks = len(track_list) if num_tracks == '' else int(num_tracks)
 
 for media in track_list:
@@ -33,3 +33,4 @@ for media in track_list:
     print(f'{media.artist} - {media.title}')
     start_time += media.length
 
+sleep(5)
