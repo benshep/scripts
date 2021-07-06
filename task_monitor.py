9b246d97dc1320f9a84f51b56a1c4facab79b652
Ben Shepherd <bjashepherd@gmail.com>
2021-07-06 09:05:52 +0100

    monitor a given task folder rather than a list of tasks

--- task_monitor.py
+++ task_monitor.py
@@ -4,12 +4,8 @@ from pushbullet_api_key import api_key  # local file, keep secret!
 
 scheduler = win32com.client.Dispatch('Schedule.Service')
 scheduler.Connect()
-root_folder = scheduler.GetFolder('\\')
-toast = ''
-for task_name in ('Change wallpaper', 'Change lockscreen background', 'Copy photos to phone', 'Update phone music'):
-    task = root_folder.GetTask(task_name)
-    if result := task.LastTaskResult != 0:
-        toast += f'{task_name}, result {hex(result & (2**32-1))}\n'
+toast = '\n'.join(f'{task.Name}, result {hex(task.LastTaskResult & (2 ** 32 - 1))}' for task in
+                  scheduler.GetFolder('Monitored').GetTasks(0) if task.LastTaskResult)
 
 if toast:
-    Pushbullet(api_key).push_note('Task Monitor', toast)
+    Pushbullet(api_key).push_note('👁️ Task Monitor', toast)
