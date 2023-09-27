6890a2ba2006fd64da130c00ca354bbbb705d43b
Ben Shepherd <bjashepherd@gmail.com>
2023-09-27 14:39:16 +0100

    title fixes

--- get_youtube_playlists.py
+++ get_youtube_playlists.py
@@ -2,73 +2,52 @@ import os
 import json
 import subprocess
 import contextlib
-from io import BytesIO
 
-import youtube_dl.utils
-from youtube_dl import YoutubeDL
+import yt_dlp.utils
+from yt_dlp import YoutubeDL
 from phrydy import MediaFile
 from media import is_media_file
-from PIL import Image
 
 
-class BadDownload(Exception):
-    pass
-
-
-class TagAdder:
+class AddTags(yt_dlp.postprocessor.PostProcessor):
     """Add tags to a post-processed file."""
     def __init__(self, album, artist):
+        super().__init__()
         self.album = album
         self.artist = artist
         self.files = []
 
-    def debug(self, msg):
-        """If a file has finished downloading, add it to the list for tagging later."""
-        # msg looks like [ffmpeg] Adding metadata to '1 Lord Franklin.opus'
-        prefix = '[ffmpeg] Adding metadata to '
-        if not msg.startswith(prefix):
-            return
-        filename = msg[len(prefix):].strip("'")
-        print('Downloaded', filename)
-        self.files.append(filename)
-
-    def add_tags(self):
-        """After all the files have finished downloading, add the tags."""
-        for filename in self.files:
-            print(f'Tagging {filename} with {self.artist = }, {self.album = }')
-            media = MediaFile(filename)
-            media.albumartist = self.artist
-            media.album = self.album
-            media.track = int(filename[:2])
-            base, _ = os.path.splitext(filename)
-            for ext in ('webp', 'jpg'):
-                art_filename = f'{base}.{ext}'
-                if os.path.exists(art_filename):
-                    if ext == 'webp':  # convert to JPEG since WEBP files can't be embedded in M4A files
-                        buffer = BytesIO()
-                        Image.open(art_filename).convert('RGB').save(buffer, 'JPEG', optimize=True)
-                        image_data = buffer.getvalue()
-                        print('Converted thumbnail to JPEG')
-                    else:
-                        image_data = open(art_filename, 'rb').read()
-                    media.art = image_data
-                    print(f'Saved thumbnail from {art_filename}')
-                    os.remove(art_filename)
-                    break
-            else:
-                print(f'No thumbnail found for {filename}')
-            media.save()
-
-    def warning(self, msg):
-        pass
-
-    def error(self, msg):
-        pass
+    def run(self, info):
+        """After a file has finished downloading, tag it with album artist and album."""
+        filename = info['filepath']
+        _, name = os.path.split(filename)
+        track = int(name.split(' ')[0])  # 01 Title.ext
+        media = MediaFile(filename)
+        media.albumartist = self.artist
+        media.album = self.album
+        media.track = track
+        title = media.title
+        if ' - ' in title:
+            # deal with titles like "Artist - Title" or "Title - Artist"
+            # (but can't automatically tell, so check first whether artist is defined
+            first, last = title.split(' - ', maxsplit=1)
+            if first == self.artist:
+                title = last
+            elif last == self.artist:
+                title = first
+        pos = title.find('Official')
+        if pos > 1:  # e.g. Song Name (Official Audio)
+            title = title[:pos].rstrip(' (-[')  # remove suffices like " - Official" and " [Official]" as well
+        title = title.strip('"')  # "Song Name" -> Song Name
+        media.title = title
+        self.to_screen(f'Tagging {name} with {self.artist = }, {self.album = }, {track=}, {title=}')
+        media.save()
+        return [], info
 
 
 def reject_large(info_dict):
     """Don't download songs longer than 15 minutes."""
-    if info_dict['duration'] < 900:
+    if info_dict.get('duration', 0) < 900:
         return None
     message = f"Too long - skipping {info_dict['title']}"
     print(message)
@@ -85,6 +64,9 @@ def get_youtube_playlists():
     user_profile = os.environ['UserProfile' if os.name == 'nt' else 'HOME']
     music_folder = os.path.join(user_profile, 'Music')
     info_file = 'download.txt'  # info file contained in each folder
+    # folder = r'K:\Music\_Copied\YouTube\Elbow\The Take Off and Landing of Everything'
+    # files = os.listdir(folder)
+    # for i in range(1):
     for folder, _, files in os.walk(music_folder):
         if info_file not in files:
             continue
@@ -96,23 +78,23 @@ def get_youtube_playlists():
                 tags = MediaFile(file)
                 artist_titles.append((tags.artist, tags.title))
 
-        def reject_existing(info_dict):
+        def reject_existing(info_dict, *args, incomplete=False):
             """Reject any videos matching existing files in the folder."""
-            for artist, title in artist_titles:
-                video_title = info_dict['title']
-                if artist in video_title and title in video_title:
-                    message = f"Already got {artist} - {title} - skipping {video_title}"
-                    print(message)
-                    return message
+            if video_title := info_dict.get('title', ''):
+                for artist, title in artist_titles:
+                    if artist and title and artist in video_title and title in video_title:
+                        message = f"Already got {artist} - {title} - skipping {video_title}"
+                        print(message)
+                        return message
             return reject_large(info_dict)  # also reject anything that's too long
 
         info = open(info_file).read()
-        if 'playlists' in info:  # playlists page
-            if not get_playlist_info(info):
-                continue
-            get_youtube_playlists()  # restart, since directory structure has changed
-            break
-        elif '{' in info:
+        # if 'playlists' in info:  # playlists page - not really using this at the mo
+        #     if not get_playlist_info(info):
+        #         continue
+        #     get_youtube_playlists()  # restart, since directory structure has changed
+        #     break
+        if '{' in info:
             playlist = json.loads(info)  # dict with keys: url, artist, album
         elif info.startswith('https://www.youtube.com/'):  # just the url?
             playlist = {'url': info.strip()}
@@ -127,28 +109,33 @@ def get_youtube_playlists():
             artist = 'Various Artists'
         if ' - ' in album_name:  # artist - album
             artist, album_name = album_name.split(' - ', 1)
-        tag_adder = TagAdder(playlist.get('album', album_name), playlist.get('artist', artist))
+        add_tags = AddTags(playlist.get('album', album_name), playlist.get('artist', artist))
         options = {'download_archive': 'download-archive.txt',  # keep track of previously-downloaded videos
+                   'force_write_download_archive': True,
+                   # 'no-warnings': True,
+                   # 'verbose': True,
                    # 'max_downloads': 1,  # for testing
                    'ignoreerrors': True, 'writethumbnail': True, 'format': 'bestaudio/best',
                    # reverse order for channels (otherwise new videos will always be track 1)
                    'playlistreverse': 'channel' in playlist['url'],
-                   # https://github.com/ytdl-org/youtube-dl/blob/master/README.md#output-template
+                   # https://github.com/yt-dlp/yt-dlp#output-template
                    'outtmpl': "%(playlist_index)02d %(title)s.%(ext)s",
+                   # 'parse_metadata': 'title:%(artist)s - %(album)s',
                    'postprocessors': [{'key': 'FFmpegExtractAudio'}, {'key': 'FFmpegMetadata'},
-                                      # {'key': 'EmbedThumbnail'}  # not supported on Opus yet - do it ourselves
+                                      {'key': 'EmbedThumbnail'}
                                       ],
-                   'logger': tag_adder, 'match_filter': reject_existing, 'progress_hooks': [show_status]}
-        with contextlib.suppress(youtube_dl.utils.MaxDownloadsReached):  # don't give an error when limit reached
-            YoutubeDL(options).download([playlist['url']])
-        # must be after everything's finished
-        tag_adder.add_tags()
+                   'match_filter': reject_existing, 'progress_hooks': [show_status]}
+        with contextlib.suppress(yt_dlp.utils.MaxDownloadsReached):  # don't give an error when limit reached
+            with YoutubeDL(options) as downloader:
+                downloader.add_post_processor(add_tags, when='after_move')
+                downloader.download([playlist['url']])
 
 
 def get_playlist_info(url):
     """Given a playlist page, download all the playlists contained there, creating sub-folders if necessary."""
+    # TODO before use: rewrite this for yt_dlp. Maybe using --print-to-file option?
     # It would be useful to use a different delimiter other than '.' here - but that doesn't seem to work
-    output = subprocess.run(['youtube-dl', '-i', '--extract-audio',
+    output = subprocess.run(['yt-dlp', '-i', '--extract-audio',
                              '--output', '"%(playlist_id)s.%(playlist)s.%(ext)s"', '--get-filename', url],
                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     lines = output.stdout.decode('utf-8').split('\n')
@@ -173,3 +160,4 @@ def get_playlist_info(url):
 
 if __name__ == '__main__':
     get_youtube_playlists()
+    # test()
