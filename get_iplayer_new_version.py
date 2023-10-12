ad01d4be421fa4f66c48747a1e3cc519fe9a28e9
Ben Shepherd <bjashepherd@gmail.com>
2023-10-12 09:02:28 +0100

    get_iplayer_new_version.py

--- get_iplayer_new_version.py
+++ get_iplayer_new_version.py
@@ -15,23 +15,24 @@ def replace_in_tag(text, tag_name, new_inner):
 
 def new_version():
     # folders
-    user_dir = os.environ['USERPROFILE']
+    user_dir = os.environ['UserProfile']
     app_name = 'get_iplayer'
     choco_name = 'getiplayer'
-    get_iplayer_folder = os.path.join(user_dir, 'GitHub', app_name)
-    wiki_folder = f'{get_iplayer_folder}.wiki'
+    app_folder = os.path.join(user_dir, 'GitHub', app_name)
+    wiki_folder = f'{app_folder}.wiki'
     choco_folder = os.path.join(user_dir, 'Documents', 'Scripts', choco_name)
     os.chdir(choco_folder)
     [send2trash(filename) for filename in os.listdir() if filename.endswith('.nupkg')]
 
     print('Updating GitHub and wiki folders')
-    assert subprocess.call('git pull', cwd=get_iplayer_folder) == 0
+    assert subprocess.call('git pull', cwd=app_folder) == 0
     assert subprocess.call('git pull', cwd=wiki_folder) == 0
 
     nuspec_filename = os.path.join(choco_folder, f'{choco_name}.nuspec')
-    nuspec = open(nuspec_filename, encoding='utf-8').read()
+    encoding = 'utf-8'
+    nuspec = open(nuspec_filename, encoding=encoding).read()
 
-    authors = open(os.path.join(get_iplayer_folder, 'CONTRIBUTORS')).read().replace('\n', ', ')[:-2]
+    authors = open(os.path.join(app_folder, 'CONTRIBUTORS')).read().replace('\n', ', ')[:-2]
     nuspec = replace_in_tag(nuspec, 'authors', authors)
 
     release_notes = open(os.path.join(wiki_folder, 'releasenotes.md')).read().splitlines()
@@ -47,11 +48,11 @@ def new_version():
             version = link_name.split(' ')[1]
             break
 
-    print(f'{app_name} version {version}')
+    print(f'{app_name} {version=}')
     nuspec = replace_in_tag(nuspec, 'version', version)
 
     # get info from readme
-    readme = open(os.path.join(get_iplayer_folder, 'README.md')).read().splitlines()
+    readme = open(os.path.join(app_folder, 'README.md')).read().splitlines()
 
     # get first two sections (title and 'Features')
     description = ''
@@ -66,7 +67,7 @@ def new_version():
     nuspec = replace_in_tag(nuspec, 'description', description)
 
     release_notes_file, link_name = link_dest.split('#')
-    release_notes = open(os.path.join(wiki_folder, f'{release_notes_file}.md'), encoding='utf-8').read().splitlines()
+    release_notes = open(os.path.join(wiki_folder, f'{release_notes_file}.md'), encoding=encoding).read().splitlines()
 
     new_release_notes = ''
     in_section = False
@@ -77,30 +78,36 @@ def new_version():
         if in_section:
             if line.startswith('<a name="'):  # next section
                 break
-            new_release_notes += line.replace('<', '`').replace('>', '`') + '\n'  # < and > need to be escaped
+            # < and > need to be escaped
+            new_release_notes += line.replace('<', '`').replace('>', '`') + '\n'
 
     print(new_release_notes)
     nuspec = replace_in_tag(nuspec, 'releaseNotes', new_release_notes)
     copyfile(nuspec_filename, f'{nuspec_filename}.bak')
-    open(nuspec_filename, 'w', encoding='utf-8').write(nuspec)
+    open(nuspec_filename, 'w', encoding=encoding).write(nuspec)
 
     # get binary info
     json = requests.get("https://api.github.com/repos/get-iplayer/get_iplayer_win32/releases/latest").content
-    release = eval(json.replace(b'false', b'False').replace(b'null', b'None').replace(b'true', b'True'))
+    release = eval(json.replace(b'false', b'False')
+                   .replace(b'null', b'None')
+                   .replace(b'true', b'True'))
 
     urls = [asset['browser_download_url'] for asset in release['assets']]
-    x86_url = next(url for url in urls if url.endswith('-x86-setup.exe'))
-    x64_url = next(url for url in urls if url.endswith('-x64-setup.exe'))
-    sha_x86_url = next(url for url in urls if '-x86-setup.exe.sha' in url)
-    sha_x86 = requests.get(sha_x86_url).content.decode('utf-8').split(' ')[0]
-    sha_x86_type = sha_x86_url.split('.')[-1]
-    sha_x64_url = next(url for url in urls if '-x64-setup.exe.sha' in url)
-    sha_x64 = requests.get(sha_x64_url).content.decode('utf-8').split(' ')[0]
-    sha_x64_type = sha_x64_url.split('.')[-1]
+
+    def get_url_sha(arch):
+        suffix = f'-x{arch}-setup.exe'
+        address = next(url for url in urls if url.endswith(suffix))
+        checksum_url = next(url for url in urls if f'{suffix}.sha' in url)
+        checksum = requests.get(checksum_url).content.decode(encoding).split(' ')[0]
+        checksum_type = checksum_url.split('.')[-1]
+        return address, checksum, checksum_type
+
+    x86_url, x86_checksum, x86_checksum_type = get_url_sha('86')
+    x64_url, x64_checksum, x64_checksum_type = get_url_sha('64')
     ps1_text = f'''
 $ErrorActionPreference = 'Stop';
 
-$packageName= 'getiplayer'
+$packageName= '{choco_name}'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = '{x86_url}'
 $url64      = '{x64_url}'
@@ -112,12 +119,12 @@ $packageArgs = @{{
   url           = $url
   url64bit      = $url64
 
-  softwareName  = 'getiplayer*'
+  softwareName  = '{choco_name}*'
 
-  checksum      = '{sha_x86}'
-  checksumType  = '{sha_x86_type}'
-  checksum64    = '{sha_x64}'
-  checksumType64= '{sha_x64_type}'
+  checksum      = '{x86_checksum}'
+  checksumType  = '{x86_checksum_type}'
+  checksum64    = '{x64_checksum}'
+  checksumType64= '{x64_checksum_type}'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }}
@@ -126,7 +133,7 @@ Install-ChocolateyPackage @packageArgs
 '''
     print('Updating the exe URL and checksums in the install file')
     install_file = os.path.join(choco_folder, 'tools', 'chocolateyinstall.ps1')
-    open(install_file, 'w', encoding='utf-8').write(ps1_text)
+    open(install_file, 'w', encoding=encoding).write(ps1_text)
 
     # package and push to server
     assert subprocess.call('choco pack', cwd=choco_folder) == 0
