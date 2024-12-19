15aa35d93b76364add854720b7cf19c980cc714e
Ben Shepherd <bjashepherd@gmail.com>
2024-12-19 14:23:15 +0000

    more folder

--- folders.py
+++ folders.py
@@ -3,3 +3,6 @@ import os
 user_profile = os.environ['UserProfile' if os.name == 'nt' else 'HOME']
 music_folder = os.path.join(user_profile, 'Music')
 docs_folder = os.path.join(user_profile, 'STFC', 'Documents')
+downloads_folder = os.path.join(user_profile, 'Downloads')
+misc_folder = os.path.join(user_profile, 'Misc')
+radio_folder = os.path.join(user_profile, 'Radio')
