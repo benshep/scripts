5514fe6081fa5cd7195ed52d9baa886af72fe4cd
Ben Shepherd <bjashepherd@gmail.com>
2025-04-22 09:38:53 +0100

    better compatibility for home folder

--- folders.py
+++ folders.py
@@ -1,8 +1,9 @@
 import os
 
-user_profile = os.environ['UserProfile' if os.name == 'nt' else 'HOME']
+user_profile = os.path.expanduser('~')
 music_folder = os.path.join(user_profile, 'Music')
 docs_folder = os.path.join(user_profile, 'STFC', 'Documents')
 downloads_folder = os.path.join(user_profile, 'Downloads')
 misc_folder = os.path.join(user_profile, 'Misc')
 radio_folder = os.path.join(user_profile, 'Radio')
+hr_info_folder = os.path.join(user_profile, 'UKRI', 'Science and Technology Facilities Council - HR')
