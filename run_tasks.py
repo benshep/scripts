8a937b756c093642b216e0f06438f4e5748e931d
Ben Shepherd <bjashepherd@gmail.com>
2025-04-23 15:17:14 +0100

    quit when unplugged

--- run_tasks.py
+++ run_tasks.py
@@ -72,6 +72,7 @@ def run_tasks():
     time_format = "%d/%m/%Y %H:%M"
     period_col = column_names.index('Period')
     pushbullet = Pushbullet(api_key)
+
     while True:
         print('Fetching data from spreadsheet')
         try:
@@ -84,87 +85,81 @@ def run_tasks():
         min_period = min(float(row[period_col]) for row in data)
         next_task_time = datetime.now() + timedelta(days=min_period)
         battery = psutil.sensors_battery()
-        if battery is None or battery.power_plugged:
-            for i in range(len(data)):
-                try:
-                    values = google_api.get_data(sheet_id, sheet_name, f'A{i + 2}:{last_col}{i + 2}')[0]
-                except Exception as e:
-                    print(e)
-                    break
-                properties = dict(zip(column_names, values))
-                if properties.get('Enabled', False) != 'TRUE':
-                    continue
-                last_result = properties.get('Last result')
-                now = datetime.now()
-                next_run_time = datetime.strptime(properties.get('Next run'), time_format)
-                if next_run_time > now and last_result in ('Success', 'Postponed'):
-                    next_task_time = min(next_task_time, next_run_time)
-                    continue
-                function_name = properties.get('Function name')
-                last_run_time = datetime.strptime(properties.get('Last run'), time_format)
-                if last_result == 'Running' and now - last_run_time < timedelta(hours=2):
-                    print(f'{function_name} already running since {last_run_time} - skipping for now')
-                    continue  # running on other PC for <2 hours - let it continue
+        if battery is not None and not battery.power_plugged:
+            print('No tasks will run on battery power. Closing.')
+            sleep(10)
+            exit()
 
-                now_str = now.strftime(time_format)
-                update_cell(i + 2, get_column('Last run'), now_str)
-                update_cell(i + 2, get_column('Machine'), node())
-                update_cell(i + 2, get_column('Last result'), 'Running')
-                parameters_raw = properties.get('Parameters', '')
-                icon = properties.get('Icon', '')
-                try:
-                    parameters = float(parameters_raw)
-                except ValueError:  # it's not a float, assume string
-                    parameters = f'"{parameters_raw}"' if parameters_raw else ''  # wrap in quotes to send to function
-                os.system(f'title {icon} {function_name}')  # set title of window
-                print('')
-                print(now_str, function_name, parameters)
-                try:
-                    return_value = eval(f'{function_name}({parameters})')
-                except Exception as exception:  # something went wrong with the task!
-                    return_value = exception
-                    error_lines = format_exc().split('\n')
-                    result = '\n'.join(error_lines[4:])
+        for i in range(len(data)):
+            try:
+                values = google_api.get_data(sheet_id, sheet_name, f'A{i + 2}:{last_col}{i + 2}')[0]
+            except Exception as e:
+                print(e)
+                break
+            properties = dict(zip(column_names, values))
+            if properties.get('Enabled', False) != 'TRUE':
+                continue
+            last_result = properties.get('Last result')
+            now = datetime.now()
+            next_run_time = datetime.strptime(properties.get('Next run'), time_format)
+            if next_run_time > now and last_result in ('Success', 'Postponed'):
+                next_task_time = min(next_task_time, next_run_time)
+                continue
+            function_name = properties.get('Function name')
+            last_run_time = datetime.strptime(properties.get('Last run'), time_format)
+            if last_result == 'Running' and now - last_run_time < timedelta(hours=2):
+                print(f'{function_name} already running since {last_run_time} - skipping for now')
+                continue  # running on other PC for <2 hours - let it continue
 
-                period = float(properties.get('Period', 1))  # default: once per day
-                next_run_time = now + timedelta(days=period)
-                match return_value:
-                    case False:  # postpone until next scheduled run
-                        result = 'Postponed'
-                    case datetime():  # postpone until specific time
-                        result = 'Postponed'
-                        next_run_time = return_value
-                    case '' | None | True:  # success but no toast
-                        result = 'Success'
-                    case str():  # success and toast summarising actions
-                        result = 'Success'
-                        print(return_value)
-                        pushbullet.push_note(f'{icon} {function_name}', return_value)
-                    case Exception():  # something went wrong with the task
-                        next_run_time = now + timedelta(days=min_period)  # try again soon
-                        split = last_result.split(' ')
-                        fail_count = int(split[1]) + 1 if split[0] == 'Failure' else 1
-                        if fail_count > 9:
-                            pushbullet.push_note('👁️ run_tasks',
-                                                 f'{function_name} failed {fail_count} times on {node()}')
-                        print(result)  # the exception traceback
-                        result = f'Failure {fail_count}'
+            now_str = now.strftime(time_format)
+            update_cell(i + 2, get_column('Last run'), now_str)
+            update_cell(i + 2, get_column('Machine'), node())
+            update_cell(i + 2, get_column('Last result'), 'Running')
+            parameters_raw = properties.get('Parameters', '')
+            icon = properties.get('Icon', '')
+            try:
+                parameters = float(parameters_raw)
+            except ValueError:  # it's not a float, assume string
+                parameters = f'"{parameters_raw}"' if parameters_raw else ''  # wrap in quotes to send to function
+            os.system(f'title {icon} {function_name}')  # set title of window
+            print('')
+            print(now_str, function_name, parameters)
+            try:
+                return_value = eval(f'{function_name}({parameters})')
+            except Exception as exception:  # something went wrong with the task!
+                return_value = exception
+                error_lines = format_exc().split('\n')
+                result = '\n'.join(error_lines[4:])
 
-                next_run_str = next_run_time.strftime(time_format)
-                print('Next run time:', next_run_str)
-                update_cell(i + 2, get_column('Next run'), next_run_str)
-                print(result)
-                update_cell(i + 2, get_column('Last result'), result)
+            period = float(properties.get('Period', 1))  # default: once per day
+            next_run_time = now + timedelta(days=period)
+            match return_value:
+                case False:  # postpone until next scheduled run
+                    result = 'Postponed'
+                case datetime():  # postpone until specific time
+                    result = 'Postponed'
+                    next_run_time = return_value
+                case '' | None | True:  # success but no toast
+                    result = 'Success'
+                case str():  # success and toast summarising actions
+                    result = 'Success'
+                    print(return_value)
+                    pushbullet.push_note(f'{icon} {function_name}', return_value)
+                case Exception():  # something went wrong with the task
+                    next_run_time = now + timedelta(days=min_period)  # try again soon
+                    split = last_result.split(' ')
+                    fail_count = int(split[1]) + 1 if split[0] == 'Failure' else 1
+                    if fail_count > 9:
+                        pushbullet.push_note('👁️ run_tasks',
+                                             f'{function_name} failed {fail_count} times on {node()}')
+                    print(result)  # the exception traceback
+                    result = f'Failure {fail_count}'
 
-        else:
-            print('On battery, not running any tasks')
-
-        # try:
-        #     set_pc_unlocked_flag()
-        # except Exception as e:
-        #     print(e)
-        #     sleep(60)
-        #     continue
+            next_run_str = next_run_time.strftime(time_format)
+            print('Next run time:', next_run_str)
+            update_cell(i + 2, get_column('Next run'), next_run_str)
+            print(result)
+            update_cell(i + 2, get_column('Last result'), result)
 
         # Sleep up to 5 minutes more than needed to avoid race conditions (two computers trying to do task at same time)
         next_task_time += timedelta(seconds=hash(node()) % 300)
