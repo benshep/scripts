5a8fee2183aa681bfd4409a21b428335d88dd98c
Ben Shepherd <bjashepherd@gmail.com>
2025-05-15 16:02:02 +0100

    find local fixtures

--- rugby_fixtures.py
+++ rugby_fixtures.py
@@ -54,6 +54,45 @@ def get_home_fixtures():
     return fixtures
 
 
+def get_local_fixtures():
+    """Get a list of fixtures to be held at the St Helens stadium using the BBC Sports API."""
+    today = datetime.today()  # - timedelta(days=3)
+    end_date = today + timedelta(weeks=12)
+    params = {'selectedEndDate': ymd(end_date),
+              'selectedStartDate': ymd(today),
+              'todayDate': ymd(today),
+              'urn': 'urn:bbc:sportsdata:rugby-league'}
+    url = 'https://www.bbc.co.uk/wc-data/container/sport-data-scores-fixtures?' + urllib.parse.urlencode(params)
+    print(url)
+    data = json.loads(requests.get(url).text)
+    fixtures = []
+    for event_group in data['eventGroups']:
+        competition = event_group['displayLabel']  # e.g. Betfred Super League
+        for sponsor in ['Betfred ']:
+            if competition.startswith(sponsor):
+                competition = competition[len(sponsor):]
+                break
+        for secondary_group in event_group['secondaryGroups']:
+            competition_round = secondary_group['displayLabel']  # e.g. Semi-final (but can be empty)
+            competition_round = f'{competition} {competition_round}' if competition_round else competition
+            # print(competition_round)
+            for match in secondary_group['events']:
+                home_team = match['home']['fullName']
+                away_team = match['away']['fullName']
+                venue = match['venue'].get('name', '')
+                # datetime format: 2025-02-15T17:30:00.000+00:00
+                start_time = datetime.strptime(match['startDateTime'], '%Y-%m-%dT%H:%M:%S.000%z')
+                # print(f'- {home_team} vs {away_team} at {venue}, {start_time}')
+                if not venue:
+                    if home_team != 'St Helens':
+                        continue
+                elif venue != 'Totally Wicked Stadium':
+                    continue
+                bbc_id = match['id']
+                fixtures.append(Fixture(bbc_id, start_time, home_team, away_team, competition_round))
+    return fixtures
+
+
 def get_calendar_events():
     now = datetime.now().isoformat() + 'Z'
     events = google_calendar.list(calendarId=calendar_id, timeMin=now, maxResults=50,
@@ -68,7 +107,7 @@ def format_time(time: datetime):
 def update_saints_calendar():
     toast = ''
     my_events = get_calendar_events()
-    for match in get_home_fixtures():
+    for match in get_local_fixtures():
         # find existing event in my calendar
         match_title = f'{match.home} vs {match.away} ({match.tournament})'
         event = {'summary': match_title,
@@ -88,4 +127,4 @@ def update_saints_calendar():
 
 
 if __name__ == '__main__':
-    print(*get_home_fixtures(), sep='\n')
+    print(*get_local_fixtures(), sep='\n')
