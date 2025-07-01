4248ca7c89832f38cf5225201f19c8e521d0eb82
Ben Shepherd <bjashepherd@gmail.com>
2025-07-01 11:42:10 +0100

    folder locations on Eddie

--- folders.py
+++ folders.py
@@ -1,9 +1,17 @@
 import os
+from platform import node
 
 user_profile = os.path.expanduser('~')
-music_folder = os.path.join(user_profile, 'Music')
-docs_folder = os.path.join(user_profile, 'STFC', 'Documents')
 downloads_folder = os.path.join(user_profile, 'Downloads')
+
+if node() == 'eddie':  # home Raspberry Pi
+    user_profile = '/media/ben/500GB'  # external drive instead of user profile
+music_folder = os.path.join(user_profile, 'Music')
 misc_folder = os.path.join(user_profile, 'Misc')
 radio_folder = os.path.join(user_profile, 'Radio')
-hr_info_folder = os.path.join(user_profile, 'UKRI', 'Science and Technology Facilities Council - HR')
+docs_folder = os.path.join(user_profile, 'STFC', 'Documents')
+if os.path.exists(docs_folder):
+    hr_info_folder = os.path.join(user_profile, 'UKRI', 'Science and Technology Facilities Council - HR')
+else:  # not on a work PC
+    docs_folder = None
+    hr_info_folder = None
