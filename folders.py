f48752e761dab221c548cc783759984b5cbe0cd5
Ben Shepherd <bjashepherd@gmail.com>
2025-09-08 19:41:26 +0100

    soft-link for Eddie instead of hardcoded paths

--- folders.py
+++ folders.py
@@ -1,11 +1,8 @@
 import os
-from platform import node
 
 user_profile = os.path.expanduser('~')
 downloads_folder = os.path.join(user_profile, 'Downloads')
 
-if node() == 'eddie':  # home Raspberry Pi
-    user_profile = '/media/ben/500GB'  # external drive instead of user profile
 music_folder = os.path.join(user_profile, 'Music')
 misc_folder = os.path.join(user_profile, 'Misc')
 radio_folder = os.path.join(user_profile, 'Radio')
