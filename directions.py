3f1ed47ef1731ad25239ee7ea11001b844604339
Ben Shepherd <bjashepherd@gmail.com>
2026-03-25 10:52:46 +0000

    directions.py

--- directions.py
+++ directions.py
@@ -3,14 +3,28 @@ from time import sleep
 
 import requests
 
+import google_api
 from google_routing_credentials import api_key
-from locations import home, katie_yr2
+import locations
 
 
-def get_directions():
-    home_lat_lng = {"location": {"latLng": home}}
-    dest = {"location": {"latLng": katie_yr2}}
+def get_directions() -> str | datetime:
+    """Get expected time and best route for driving home."""
+    # wait until late afternoon
+    now = datetime.now()
+    # if now.hour < 16:
+    #     return now.replace(hour=16, minute=0)
+    sheet_id = '1GzhMCHv6vgi9R2LNhmdKg7Rjy967tC6XrqFBJGxcGXM'  # Exercise spreadsheet
+    last_ride_date, last_ride_route = google_api.get_data(sheet_id, 'Cycling', 'lastRide')[0]
+    last_ride_date = datetime.strptime(last_ride_date, "%d/%m/%Y %H:%M:%S")
+    if last_ride_date.date() == now.date() and 'to work' in last_ride_route:  # it's a cycle day - don't need driving route
+        return (now + timedelta(days=1)).replace(hour=16, minute=0)
+    home = {"location": {"latLng": locations.home}}
+    work = {"location": {"latLng": locations.bike_shed}}
     url = 'https://routes.googleapis.com/directions/v2:computeRoutes'
+    km_cost = 0.107
+    bridge_cost = 2.15
+    hourly_rate = 4
 
     headers = {
         "Content-Type": "application/json",
@@ -19,28 +33,36 @@ def get_directions():
     }
 
     payload = {
-        "origin": home_lat_lng,
-        "destination": dest,
-        "computeAlternativeRoutes": False,
+        "origin": work,
+        "destination": home,
+        "computeAlternativeRoutes": True,
         "routingPreference": "TRAFFIC_AWARE_OPTIMAL"
     }
 
-    while True:
-        response = requests.post(url, json=payload, headers=headers).json()
-        for route in response['routes']:
-            duration = timedelta(seconds=int(route['duration'].rstrip('s')))
-            km = route['distanceMeters'] / 1000
-            now = datetime.now()
-            eta = now + duration
-            print(now.strftime('%H:%M'),
-                  str(duration)[:-3],  # don't need seconds
-                  f"ETA {eta.strftime('%H:%M')}",
-                  f"{km:.0f} km",
-                  route['description'],
-                  *route['warnings'],
-                  sep='\t')
-        sleep(60 * 5)
-
+    response = requests.post(url, json=payload, headers=headers).json()
+    for route in response['routes']:
+        route['duration'] = timedelta(seconds=int(route['duration'].rstrip('s')))
+        route['km'] = route['distanceMeters'] / 1000
+        route['eta'] = now + route['duration']
+        route['cost'] = km_cost * route['km']
+        if 'This route has tolls.' in route['warnings']:
+            route['cost'] += bridge_cost
+        route['weighted_cost'] = route['cost'] + route['duration'].total_seconds() * hourly_rate / 3600
+    response['routes'] = sorted(response['routes'], key=lambda route: route['weighted_cost'])
+    for route in response['routes']:
+        print(now.strftime('%H:%M'),
+              str(route['duration'])[:-3],  # don't need seconds
+              f"ETA {route['eta'].strftime('%H:%M')}",
+              f"£{route['cost']:.2f}",
+              f"£{route['weighted_cost']:.2f}",
+              f"{route['km']:.0f} km",
+              route['description'],
+              *route['warnings'],
+              sep='\t')
+    route = response['routes'][0]
+    return f"ETA {route['eta'].strftime('%H:%M')} via {route['description']}"
 
 if __name__ == '__main__':
-    get_directions()
+    while True:
+        print(get_directions())
+        sleep(60 * 5)
